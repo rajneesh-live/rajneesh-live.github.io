b1419fe5c08ffce922166e7bfa0826dcd7073a62
Omkar Tenkale <omkar.tenkale@deliveroo.com>
2026-02-20 00:27:47 +0530

    Force redirect away from home page

--- src/routes/(marketing)/+page.ts
+++ src/routes/(marketing)/+page.ts
@@ -1,5 +1,10 @@
+import { redirect } from '@sveltejs/kit'
 import '../../app.css'
 
 export const ssr = true
 export const prerender = true
 export const csr = true
+
+export const load = () => {
+	redirect(301, '/library/shorts')
+}
